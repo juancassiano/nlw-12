9dde08665e450976ac6df3bdd168f8f1fcf7dacb
juancassiano <juancassiano@hotmail.com>
2023-05-17 16:11:10 -0300

    crete frontend interface

--- nextjs-web/src/app/page.tsx
+++ nextjs-web/src/app/page.tsx
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      <div className="relative flex flex-col items-center justify-between overflow-hidden border-r border-white/10 px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+      </div>
+
+      <div className="flex flex-col bg-[url('../assets/bg-stars.svg')] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, começa{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }

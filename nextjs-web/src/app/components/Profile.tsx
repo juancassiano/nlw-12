81d919f31588898c5b9e1fdf4d00bf1678e9cb79
juancassiano <juancassiano@hotmail.com>
2023-05-19 16:26:07 -0300

    Criando nova memória no mobile, e criando autorização no front

--- nextjs-web/src/app/components/Profile.tsx
+++ nextjs-web/src/app/components/Profile.tsx
@@ -1,5 +1,6 @@
 import { getUser } from '@/lib/auth'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function Profile() {
   const { name, avatarUrl } = getUser()
@@ -15,9 +16,12 @@ export function Profile() {
 
       <p className="max-w[140px] text-sm leading-snug">
         {name}
-        <a href="" className="block text-red-400 hover:text-red-300">
+        <Link
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
           Quero Sair
-        </a>
+        </Link>
       </p>
     </div>
   )
